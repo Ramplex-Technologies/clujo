be170860dbefae94b2daf47efcbd59bdd5ef0b0c
Rami Pellumbi <98100379+ramipellumbi@users.noreply.github.com>
2024-10-16 00:05:54 -0400

    Fix scheduler start not containing completionHandler

--- src/scheduler.ts
+++ src/scheduler.ts
@@ -3,7 +3,7 @@ import type { IClujo } from "./clujo.types";
 
 export class Scheduler {
   // biome-ignore lint/suspicious/noExplicitAny: handle any combination of clujo's
-  private readonly jobs: { job: IClujo<any, any, any>; completionHandler: (ctx: any) => Promise<void> | void }[] = [];
+  private readonly jobs: {job: IClujo<any, any, any>; completionHandler: (ctx: any) => Promise<void> | void; }[] = [];
 
   addJob<TDependencies, TContext>(input: {
     // biome-ignore lint/suspicious/noExplicitAny: I do not want to type this
@@ -14,8 +14,8 @@ export class Scheduler {
   }
 
   start(redis?: Redis) {
-    for (const { job } of this.jobs) {
-      job.start({ redis });
+    for (const { job, completionHandler } of this.jobs) {
+      job.start({ redis, completionHandler });
     }
   }
 
