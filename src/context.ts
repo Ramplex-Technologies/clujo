289031f16cf2f2a011ba433fc1c49f0ba8e8e2bf
Rami Pellumbi <98100379+ramipellumbi@users.noreply.github.com>
2024-10-16 00:06:06 -0400

    Add comments to context

--- src/context.ts
+++ src/context.ts
@@ -1,3 +1,6 @@
+/**
+ * Used to allow for the sharing of state between tasks.
+ */
 export class Context<T extends object> {
   private object!: { initial: object | undefined } & T;
   private updateQueue: Promise<void>;
@@ -7,19 +10,32 @@ export class Context<T extends object> {
     this.updateQueue = Promise.resolve();
   }
 
+  /**
+   * Gets the current state of the managed object.
+   */
   public get value(): { initial: object | undefined } & T {
     return this.object;
   }
 
+  /**
+   * Resets the context to its initial state or a new initial object.
+   */
   public reset(initialObject?: object): void {
     if (initialObject) {
-      this.object = { initial: { ...initialObject } } as { initial: object | undefined } & T;
+      this.object = { initial: { ...initialObject } } as {
+        initial: object | undefined;
+      } & T;
     } else {
-      this.object = { initial: undefined } as { initial: object | undefined } & T;
+      this.object = { initial: undefined } as {
+        initial: object | undefined;
+      } & T;
     }
   }
 
-  update<NewValue extends object>(updateValue: NewValue): Promise<void> {
+  /**
+   * Asynchronously updates the context with new values. Ensures that updates are applied in the order they are called.
+   */
+  public update<NewValue extends object>(updateValue: NewValue): Promise<void> {
     this.updateQueue = this.updateQueue.then(() => {
       // overrides won't happen with how this is used since
       // the initial context is under the key "initial"
