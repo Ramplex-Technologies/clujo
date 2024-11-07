db1a383ac1f7cbf9dc6d510843fcbca4d754075f
Rami Pellumbi <ramipellumbi@gmail.com>
2024-11-07 14:20:11 -0500

    Add deep freeze to context

--- src/_context.ts
+++ src/_context.ts
@@ -38,20 +38,20 @@ export class Context<TInitial, TContext> {
     /**
      * Gets the current state of the managed object.
      */
-    public get value(): { initial: TInitial | undefined } & TContext {
+    get value(): { initial: TInitial | undefined } & TContext {
         return this.object;
     }
 
     /**
      * Resets the context to its initial state or a new initial object.
      */
-    public reset(initialValue: TInitial | undefined): void {
+    reset(initialValue: TInitial | undefined): void {
         if (initialValue !== undefined && initialValue !== null) {
-            this.object = { initial: initialValue } as {
+            this.object = deepFreeze({ initial: initialValue }) as {
                 initial: TInitial;
             } & TContext;
         } else {
-            this.object = { initial: undefined } as {
+            this.object = deepFreeze({ initial: undefined }) as {
                 initial: TInitial | undefined;
             } & TContext;
         }
@@ -60,14 +60,28 @@ export class Context<TInitial, TContext> {
     /**
      * Asynchronously updates the context with new values. Ensures that updates are applied in the order they are called.
      */
-    public update<NewValue extends object>(updateValue: NewValue): Promise<void> {
+    update<NewValue extends object>(updateValue: NewValue): Promise<void> {
         this.updateQueue = this.updateQueue.then(() => {
             // overrides won't happen with how this is used since
             // the initial context is under the key "initial"
             // and all task results are under the unique id of that task
-            this.object = { ...this.object, ...updateValue };
+            this.object = deepFreeze({ ...this.object, ...updateValue });
             return Promise.resolve();
         });
         return this.updateQueue;
     }
+}
+
+// prevent runtime modifications to the context
+function deepFreeze<T extends object>(obj: T): Readonly<T> {
+    const propNames = Reflect.ownKeys(obj) as (keyof T)[];
+
+    for (const name of propNames) {
+        const value = obj[name];
+        if (value && typeof value === "object" && !Object.isFrozen(value)) {
+            deepFreeze(value);
+        }
+    }
+
+    return Object.freeze(obj);
 }
